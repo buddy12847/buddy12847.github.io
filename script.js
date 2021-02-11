f69339658dae60a165e0941e7796eec455dc938e
TheOneTold <74574842+TheOneTold@users.noreply.github.com>
2021-02-11 11:15:53 -0600

    Added holidays

--- script.js
+++ script.js
@@ -81,7 +81,138 @@ if(tzoff!=12 && currentHour < 6){
 	
 }
 
-// Below exports the day formatted to the correct format into a paragraph with the id of "p1".
+if (month == "G" && day == 1){
+	document.getElementById("p2").innerHTML = "Happy Garfunkel and Paw Wah Day! We wish you a happy Garfunkel Year " + year + "!";
+}
+if (month == "G" && day == 6){
+	document.getElementById("p2").innerHTML = "Happy Garfunkian Independence Day!";
+}
+if (month == "G" && day == 16){
+	document.getElementById("p2").innerHTML = "Happy Racism Day!";
+}
+if (month == "G" && day == 16){
+	document.getElementById("p2").innerHTML = "Happy Jesse Dotson Day! He is " + (currentYear - 2005) + " years old today.";
+}
+if (month == "G" && day == 59){
+	document.getElementById("p2").innerHTML = "Happy Convert a Sex Worker Day!";
+}
+if (month == "L" && day == 8){
+	document.getElementById("p2").innerHTML = "Happy Reviewbrah Day and Summer Solstice!";
+}
+if (month == "L" && day == 10){
+	document.getElementById("p2").innerHTML = "Happy Hardee's Day! Drop by and order a bacon, egg, and cheese biscuit.";
+}
+if (month == "L" && day == 22){
+	document.getElementById("p2").innerHTML = "Happy Seinfeld Day!";
+}
+if (month == "L" && day == 36){
+	document.getElementById("p2").innerHTML = "Happy Henrald Legg Day! He is " + (currentYear - 2004) + " years old today.";
+}
+if (month == "L" && day == 50){
+	document.getElementById("p2").innerHTML = "Happy Aidan Orth Day! He is " + (currentYear - 2004) + " years old today.";
+}
+if (month == "L" && day == 56){
+	document.getElementById("p2").innerHTML = "Happy Andrew Davidson Day! He is " + (currentYear - 2005) + " years old today.";
+}
+if (month == "L" && day == 59){
+	document.getElementById("p2").innerHTML = "Today we remember the loss of Terry Davis " + (currentYear - 2018) + " years ago today.";
+}
+if (month == "L" && day == 63){
+	document.getElementById("p2").innerHTML = "War of the Bronies Rememberance Day. " + (currentYear - 2018) + " years ago today, our brave men fought one of the greatest battles in Garfunkian history.";
+}
+if (month == "L" && day == 68){
+	document.getElementById("p2").innerHTML = "Happy Afghan Independence Day! The first ally of Garfunkia.";
+}
+if (month == "P" && day == 20){
+	document.getElementById("p2").innerHTML = "Epic 9/11 Day.";
+}
+if (month == "P" && day == 26){
+	document.getElementById("p2").innerHTML = "Happy Curious George Day!";
+}
+if (month == "P" && day == 31){
+	document.getElementById("p2").innerHTML = "Happy Fall Equinox!";
+}
+if (month == "P" && day == 37){
+	document.getElementById("p2").innerHTML = "Happy Rabies Day!";
+}
+if (month == "P" && day == 41){
+	document.getElementById("p2").innerHTML = "Today is the first day of the Week of Paw Wah. We wish you a merry week of celebrations!";
+}
+if (month == "P" && day == 50){
+	document.getElementById("p2").innerHTML = "Today is the last day of the Week of Paw Wah, we hope you had a great week with your friends and family.";
+}
+if (month == "D" && day == 17){
+	document.getElementById("p2").innerHTML = "Happy Doug Walker Day! He is " + (currentYear - 1981) + " years old today.";
+}
+if (month == "D" && day == 20){
+	document.getElementById("p2").innerHTML = "Happy Raphtalia Conversion Day!";
+}
+if (month == "D" && day == 20){
+	document.getElementById("p2").innerHTML = "Happy スーパードンキーコング2 Day!";
+}
+if (month == "D" && day == 28){
+	document.getElementById("p2").innerHTML = "Happy Zach Dawson Day! He is " + (currentYear - 2004) + " years old today.";
+}
+if (month == "D" && day == 44){
+	document.getElementById("p2").innerHTML = "Happy Alvin and the Chipmunks Day!";
+}
+if (month == "D" && day == 45){
+	document.getElementById("p2").innerHTML = "Happy Nicholas Qualls and Terry Davis Day! They are " + (currentYear - 2004) + " and " + (currentYear - 1969) + " years old today respectively.";
+}
+if (month == "D" && day == 49){
+	document.getElementById("p2").innerHTML = "Happy Tale of Despereaux Day!";
+}
+if (month == "D" && day == 51){
+	document.getElementById("p2").innerHTML = "Happy Winter Solstice!";
+}
+if (month == "D" && day == 57){
+	document.getElementById("p2").innerHTML = "Happy Anti-Woodrow Wilson Day!";
+}
+if (month == "D" && day == 70){
+	document.getElementById("p2").innerHTML = "Happy I am not a crook Day! Richard Nixon is " + (currentYear - 1913) + " years old today.";
+}
+if (month == "C" && day == 1){
+	document.getElementById("p2").innerHTML = "Happy Tintin Day!";
+}
+if (month == "C" && day == 4){
+	document.getElementById("p2").innerHTML = "Happy Explosives Day!";
+}
+if (month == "C" && day == 20){
+	document.getElementById("p2").innerHTML = "Happy Anti-Kansas Day! The worst state.";
+}
+if (month == "C" && day == 21){
+	document.getElementById("p2").innerHTML = "Happy Quick Knowledge Day! Teach us , from the baby nursery please Or after few days on the way , we will be pulling our hair.";
+}
+if (month == "C" && day == 30){
+	document.getElementById("p2").innerHTML = "Happy Misa Day!";
+}
+if (month == "C" && day == 33){
+	document.getElementById("p2").innerHTML = "Happy Alex Jones Day! He is " + (currentYear - 1974) + " years old today.";
+}
+if (month == "C" && day == 37){
+	document.getElementById("p2").innerHTML = "Happy Professor Layton Day!";
+}
+if (month == "C" && day == 40){
+	document.getElementById("p2").innerHTML = "Happy Chad Day! He is " + (currentYear - 2005) + " years old today.";
+}
+if (month == "C" && day == 62){
+	document.getElementById("p2").innerHTML = "Happy Scatman Day! He is " + (currentYear - 1942) + " years old today.";
+}
+if (month == "C" && day == 67){
+	document.getElementById("p2").innerHTML = "Today marks the anniversary of the founding of the Knights of Gangweed.";
+}
+if (month == "C" && day == 70){
+	document.getElementById("p2").innerHTML = "Happy Spring Equinox!";
+}
+if (month == "R" && day == 1){
+	document.getElementById("p2").innerHTML = "Today marks the first day of the month of Repentance. Repent your sins to Garfunkel as the year comes to a close.";
+}
+if (month == "R" && day == 15){
+	document.getElementById("p2").innerHTML = "Today marks the final day of the month of Repentance and New Year's Eve. Garfunkel Year " + year + " comes to an end today as we enter into a new year.";
+}
+
+// Below
+// exports the day formatted to the correct format into a paragraph with the id of "p1".
 document.getElementById("p1").innerHTML = month + day + "-GY" + year;
 function sendMessage() {
       var request = new XMLHttpRequest();
