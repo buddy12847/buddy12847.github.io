44addf00cfecfe553dfb9e2ce31130a14db32c1b
Garfunkia <iandains11@gmail.com>
2021-06-26 20:59:22 -0500

    update to shadows, updated main section on compendium

--- compendium/infobox.js
+++ compendium/infobox.js
@@ -2,9 +2,11 @@ function openNav() {
 	document.getElementById("infobox").style.display = "revert";
 	document.getElementById("dropdown").style.display = "none";
 	document.getElementById("dropdown2").style.display = "block";
+	document.getElementById("main").style.display = "none";
 }
 function openNav2() {
 	document.getElementById("infobox").style.display = "none";
 	document.getElementById("dropdown").style.display = "block";
 	document.getElementById("dropdown2").style.display = "none";
+	document.getElementById("main").style.display = "revert";
 }
